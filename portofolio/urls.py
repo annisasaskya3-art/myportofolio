72b99918cd56455017811dd7f5fa907f431a82d5
Annisa Saskya Aulia <annisasaskya3@gmail.com>
2026-09-07 15:50:17 +0700

    fix: resolve python-dotenv dependency and complete tutorial 01 setup

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from portofolio.views import landing_page
+from portofolio.views import landing_page 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing_page, name='landing_page'),
+    path('', landing_page, name='landing_page'), 
 ]
