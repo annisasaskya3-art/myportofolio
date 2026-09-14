dbfaa682cf8c8a0035427779395f4bb92fb11b17
Annisa Saskya Aulia <annisasaskya3@gmail.com>
2026-09-14 21:49:44 +0700

    feat: implement Project model (menu) and MVT flow for projects page

--- main/models.py
+++ main/models.py
@@ -27,3 +27,12 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Project(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=200)
+    project_url = models.URLField(blank=True, null = True)
+
+    def  __str__ (self):
+        return self.title
